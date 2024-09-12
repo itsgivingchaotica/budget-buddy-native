ce2141b17939497bd9a128b35a2e2254e2ff11d8
Saoirse Siobhan Ebert <91578619+itsgivingchaotica@users.noreply.github.com>
2024-09-11 21:28:03 -0400

    place kinde creds in env file

--- utils/KindeConfig.ts
+++ utils/KindeConfig.ts
@@ -1,8 +1,8 @@
 import { KindeSDK } from "@kinde-oss/react-native-sdk-0-7x";
 
 export const client = new KindeSDK(
-  "https://budgetbuddyapp.kinde.com",
-  "exp://192.168.1.22:8081",
-  "5decc948881744a984706643a3e70b15",
-  "exp://192.168.1.22:8081"
+  process.env.EXPO_PUBLIC_KINDE_APP_DOMAIN,
+  process.env.EXPO_PUBLIC_KINDE_ALLOWED_CALLBACK_URL,
+  process.env.EXPO_PUBLIC_KINDE_CLIENT_ID,
+  process.env.EXPO_PUBLIC_KINDE_LOGOUT_REDIRECT_URL
 );
