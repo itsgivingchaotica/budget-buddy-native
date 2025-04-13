952b1854d00502de859714d079d3cb065f6fad34
Saoirse Siobhan Ebert <91578619+itsgivingchaotica@users.noreply.github.com>
2025-04-13 17:39:47 -0400

    receive both tag namd and tag id via categorytag component

--- components/CategoryTags/CategoryTag.tsx
+++ components/CategoryTags/CategoryTag.tsx
@@ -2,17 +2,32 @@ import { View, Text, TouchableOpacity } from "react-native";
 import React from "react";
 import { useRouter } from "expo-router";
 
-export const CategoryTag: React.FC = ({ tag }: { tag: string }) => {
+export const CategoryTag: React.FC = ({
+  tagId,
+  tagName,
+  categoryId,
+}: {
+  tagId: number;
+  tagName: string;
+  categoryId: number;
+}) => {
   const router = useRouter();
 
+  console.log(categoryId, " category id");
+  console.log(tagId, " the tag id");
+
   const handleNavigate = () => {
-    router.push(`/category/new-entry?tag=${encodeURIComponent(tag)}`);
+    router.push(
+      `/category/new-entry?tagId=${encodeURIComponent(
+        tagId
+      )}&tagName=${encodeURIComponent(tagName)}`
+    );
   };
 
   return (
     <TouchableOpacity onPress={handleNavigate}>
       <View className="bg-white rounded-lg shadow-lg p-4 m-2">
-        <Text className="text-xl font-bold">{tag}</Text>
+        <Text className="text-xl font-bold">{tagName}</Text>
       </View>
     </TouchableOpacity>
   );
